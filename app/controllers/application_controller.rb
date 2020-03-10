8b93ecd9001dca184808e09b4697e0ed67caad20
Nina Skyttmo <nina@dynur.com>
2020-03-10 11:42:46 +0100

    create blank controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  #before_action
+
+  def index
+    # As a user, I can list tasks
+  end
+
+  def show
+  # As a user, I can view the details of a task
+  end
+
+  def new
+  # As a user, I can add a new task
+  end
+
+  def edit
+  # As a user, I can edit a task (mark as completed / update title & details)
+  end
+
+  def destroy
+  # As a user, I can remove a task
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
